0d8c97ef261a9030052060ea0bfc3b0658852b8a
Thomasns <THOMASNS@ETSU.EDU>
2020-12-06 02:16:18 -0500

    Day 6

---
+++ AoC2020Day6/Helpers.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace AoC2020Day6
+{
+    class Helpers
+    {
+        public static String readToBuffer(int day)
+        {
+            StreamReader sr = new StreamReader(@"C:\Users\thoma\Documents\AoC\input\2020\day" + day + @".txt");
+            string input = sr.ReadToEnd();
+            sr.Close();
+            return input;
+        }
+
+        public static List<int> readToIntList(int day, char delim = '\n')
+        {
+            String strIn = readToBuffer(day);
+            String[] parts = strIn.Trim().Split(delim);
+
+            List<int> lout = new List<int>();
+            foreach (string s in parts)
+                lout.Add(int.Parse(s));
+            return lout;
+
+        }
+
+        public static List<String> readToStringList(int day, char delim = '\n')
+        {
+            String strIn = readToBuffer(day);
+            String[] parts = strIn.Trim().Split(delim);
+
+            return parts.ToList<String>();
+
+
+        }
+
+        public static List<string> readAsBlankSeparatedBlocks(int day, char delim = '\n'  )
+        {
+            String strIn = readToBuffer(day);
+            String[] parts = strIn.Trim().Split(delim);
+
+            List<string> blocks = new List<string>();
+            string line = "";
+            for(int i = 0; i < parts.Length; i++)
+            {
+                if (parts[i].Trim() == "")
+                {
+                    blocks.Add(String.Copy(line));
+                    line = "";
+
+                }
+                else
+                {
+                    line += parts[i];
+                }
+            }
+            blocks.Add(line);
+
+            return blocks;
+            
+
+        }
+
+
+
+
+
+    }
+}
---
+++ AoC2020Day6/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AoC2020Day6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<String> blocks = Helpers.readAsBlankSeparatedBlocks(6);
+
+            int groupCount = 0;
+            int total = 0;
+            for (int i = 0; i < blocks.Count; i++)
+            {
+                groupCount = 0;
+                for (char c = 'a'; c <= 'z'; c++)
+                {
+                    if (blocks[i].Contains(c))
+                        groupCount++;
+                }
+                total += groupCount;
+            }
+            Console.WriteLine("Part 1:" + total);
+
+            total = 0;
+            for (int i = 0; i < blocks.Count; i++)
+            {
+                groupCount = 0;
+                string[] answers = blocks[i].Trim().Split('\r');
+                for (char c = 'a'; c <= 'z'; c++)
+                {
+                    bool all = true;
+                    for (int j = 0; j < answers.Length; j++)
+                    {
+                        if (!answers[j].Contains(c))
+                        {
+                            all = false;
+                            break;
+                        }
+                    }
+                    if (all)
+                        groupCount++;
+                }
+                total += groupCount;
+            }
+            Console.WriteLine("Part 2: " + total);
+            Console.ReadLine();
+        }
+    }
+}
