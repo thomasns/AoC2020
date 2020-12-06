6cb1c4e729bb21688da55adc8f774aa6390504d7
Thomasns <THOMASNS@ETSU.EDU>
2020-12-05 20:14:23 -0500

    Day 5

---
+++ AoC2020Day5/Helpers.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace AoC2020Day5
+{
+    class Helpers
+    {
+        public static String readToBuffer(int day)
+        {
+            StreamReader sr = new StreamReader(@"C:\Users\thoma\Documents\AoC\input\2020\day" + day + @".txt");
+            string input = sr.ReadToEnd();
+            return input;
+        }
+
+        public static List<int> readToIntList(int day, char delim = '\n')
+        {
+            String strIn = readToBuffer(day);
+            String[] parts = strIn.Trim().Split(delim);
+
+            List<int> lout = new List<int>();
+            foreach (string s in parts)
+                lout.Add(int.Parse(s));
+            return lout;
+
+        }
+
+        public static List<String> readToStringList(int day, char delim = '\n')
+        {
+            String strIn = readToBuffer(day);
+            String[] parts = strIn.Trim().Split(delim);
+
+            return parts.ToList<String>();
+
+
+        }
+
+
+
+
+
+    }
+}
---
+++ AoC2020Day5/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AoC2020Day5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sample = "BFFFBBFRRR";
+            List<string> lin = Helpers.readToStringList(5);
+            int maxSeat = 0;
+
+            foreach (string line in lin)
+            {
+                if(checksum(line) > maxSeat)
+                    maxSeat = checksum(line);
+            }
+            Console.WriteLine(maxSeat);
+
+
+            bool[,] seats = new bool[128, 8];
+            List<int> takenSeats = new List<int>();
+            foreach (string line in lin)
+            {
+                plugArray(seats,line);
+                takenSeats.Add(checksum(line));
+            }
+
+            for(int i = 0; i < 128; i++)
+            {
+                for(int j = 0; j < 8; j++)
+                {
+                    if (!seats[i, j])
+                    {
+                        int value = i * 8 + j;
+                        if(takenSeats.Contains(value-1) && takenSeats.Contains(value+1))
+                        {
+                            Console.WriteLine(value);
+                        }
+
+                    }
+                }
+            }
+            Console.ReadLine();
+        }
+
+
+        static void plugArray(bool[,] seats, string lin)
+        {
+            int row = findRow(lin.Substring(0, 7));
+            int column = findRow(lin.Substring(7, 3), 'R');
+
+            seats[row, column] = true;
+
+            
+        }
+        
+        static int checksum(string lin)
+        {
+            int row = findRow(lin.Substring(0, 7));
+            int column = findRow(lin.Substring(7, 3), 'R');
+
+            return row * 8 + column;
+        }
+    
+        static int findRow(string sin, char onBit = 'B')
+        {
+            int value = 0;
+
+            for(int i = 0; i < sin.Length;  i++)
+            {
+                if (sin[i] == onBit) 
+                    value += 1;
+                value = value << 1;
+            }
+            value = value >> 1;
+
+            return value;
+        }
+    }
+}
